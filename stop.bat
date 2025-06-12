255d09aca34e058766d1a4937744aab4ff495348
Shubhranshu Rauta <shrauta@microsoft.com>
2025-06-12 10:56:57 +0530

    Add screenshot

--- stop.bat
+++ stop.bat
@@ -2,7 +2,7 @@
 tasklist /FI "IMAGENAME eq screenshot_loop.exe" | find /I "screenshot_loop.exe" >nul
 if %ERRORLEVEL%==0 (
     echo Process is running. Killing it...
-    taskkill /IM screenshot_queue.exe /F
+    taskkill /IM screenshot_loop.exe /F
 ) else (
     echo Process not found. Nothing to kill.
 )
