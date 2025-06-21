8c0f562240ce267b89865ed8e500ee9df98b7b3d
shubha28mca <shubha28mca@gmail.com>
2025-06-21 23:30:58 +0530

    Add screenshot

--- check.bat
+++ check.bat
@@ -1,3 +1,3 @@
 @echo off
 tasklist /FI "IMAGENAME eq screenshot_loop.exe" | find /I "screenshot_loop.exe"
-tasklist /FI "IMAGENAME eq client.exe" | find /I "client.exe"
+tasklist /FI "IMAGENAME eq screenshot_client.exe" | find /I "screenshot_client.exe"
