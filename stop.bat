67ab7fec503fb7d19af2ac7028a15d1ba92d19ef
shubha28mca <shubha28mca@gmail.com>
2025-06-22 16:47:59 +0530

    Add screenshot

--- stop.bat
+++ stop.bat
@@ -6,3 +6,12 @@ if %ERRORLEVEL%==0 (
 ) else (
     echo Process not found. Nothing to kill.
 )
+
+
+tasklist /FI "IMAGENAME eq screenshot_client.exe" | find /I "screenshot_client.exe" >nul
+if %ERRORLEVEL%==0 (
+    echo Process is running. Killing it...
+    taskkill /IM screenshot_client.exe /F
+) else (
+    echo Process not found. Nothing to kill.
+)
